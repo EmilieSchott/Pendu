ed2af92659675472163415d1955074abd4bc88cc
Emilie Schott <emilie.schott@gmail.com>
2024-05-24 19:07:38 +0200

    Add timer to avoid that the window closes before the player can read the game conclusion.

--- Pendu/src/main.cpp
+++ Pendu/src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 #include "../include/word.h"
 #include "../include/letter.h"
 #include "../include/constants.h"
@@ -30,12 +32,15 @@ int main()
 	}
 
 	if (errorsNumber == ALLOWDED_ERRORS_NUMBER) {
-		cout << "Vous avez perdu ! Le mot etait : " << wordToGuess;
+		cout << "Vous avez perdu ! Le mot etait : " << wordToGuess << endl;
 	}
 
 	if (errorsNumber < ALLOWDED_ERRORS_NUMBER && wordToGuess == wordState) {
 		cout << "Felicitations ! Vous avez gagne !" << endl;
 	}
+
+	std::this_thread::sleep_for(10ns);
+	std::this_thread::sleep_until(std::chrono::system_clock::now() + 10s);
 
 	return EXIT_SUCCESS;
 }
